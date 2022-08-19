80a85c9a08bae86ff38f5b0271e6c123d3487ea6
tcampbell01 <teresa.j.campbell@gmail.com>
2022-08-19 15:13:11 -0400

    strings in java

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println(myString);
+        myString = myString + "\u00A9 2019";
+        System.out.println(myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        //prints out 250.5549.95
+
+
+
+    }
+}
